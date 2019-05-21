d4739ae2bab4413d18123da33ed8739858025b0b
dgupta777 <darshitgupta.cse20@jecrc.ac.in>
2019-05-21 14:14:56 +0530

    add directly via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBGJRrC7vk-9ZuJ-blYpE3KnlZIhjttx1s",
+    authDomain: "todolistapp-e7494.firebaseapp.com",
+    databaseURL: "https://todolistapp-e7494.firebaseio.com",
+    projectId: "todolistapp-e7494",
+    storageBucket: "todolistapp-e7494.appspot.com",
+    messagingSenderId: "754754130428",
+    appId: "1:754754130428:web:02bf4d1b1d0e1211"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBGJRrC7vk-9ZuJ-blYpE3KnlZIhjttx1s",
+    authDomain: "todolistapp-e7494.firebaseapp.com",
+    databaseURL: "https://todolistapp-e7494.firebaseio.com",
+    projectId: "todolistapp-e7494",
+    storageBucket: "todolistapp-e7494.appspot.com",
+    messagingSenderId: "754754130428",
+    appId: "1:754754130428:web:02bf4d1b1d0e1211"
+  }
 };
 
 /*
